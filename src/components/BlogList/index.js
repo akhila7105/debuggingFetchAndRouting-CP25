6aa7266647d4b85bcf49e1b1c6b3b73165936d30
akhila7105 <akhilakoripelli21@gmail.com>
2023-05-29 03:18:36 +0000

    debuggingFetchAndRouting-CP25

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,58 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogList extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  // wait should always be used inside async function
+
+  getBlogsData = async () => {
+    //   spelling of fetch
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    // To get the response in json format we need to call response.json() method
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    // Response received for blogsData should be updated in the state
+    this.setState({blogsData: formattedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blogs-list-container">
+        {isLoading ? (
+          // eslint-disable-next-line react/no-unknown-property
+          <div testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          <ul className="blogs-list">
+            {blogsData.map(eachBlogItem => (
+              <BlogItem key={eachBlogItem.id} blogItemDetails={eachBlogItem} />
+            ))}
+            {/* FIX17: When rendering a list of items we need to use "key" prop */}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
